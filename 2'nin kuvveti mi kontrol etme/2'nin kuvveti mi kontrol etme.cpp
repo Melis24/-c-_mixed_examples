c01d4b36ecd89725ae7c8170e511024d34587fa1
Melis24 <77929541+Melis24@users.noreply.github.com>
2022-02-12 16:42:11 +0300

    2'nin kuvveti kontrol etme

---
+++ 2'nin kuvveti mi kontrol etme/2'nin kuvveti mi kontrol etme.cpp
@@ -0,0 +1,43 @@
+﻿// 2'nin kuvveti mi kontrol etme.cpp : Bu dosya 'main' işlevi içeriyor. Program yürütme orada başlayıp biter.
+//
+
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    
+    int number;
+    bool control =false;
+
+    cout << "Enter to a number:";
+    cin >> number;
+
+    while (number >1)
+    {
+       
+        if (number % 2== 0)
+        {
+            number /= 2;
+        }
+        else
+        {
+            control = true;
+            break;
+        }
+
+    }
+
+    if (control)
+    {
+        cout << "The number entered is not a power of two.";
+    }
+    else
+    {
+        cout << "The number entered is a power of two.";
+    }
+
+
+}
+
