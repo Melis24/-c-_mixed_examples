71d077a8fe029d2c63bf781df21d95f952bf514d
Melis24 <77929541+Melis24@users.noreply.github.com>
2022-02-14 17:46:37 +0300

    Basamak degeri toplama

---
+++ Basamak değeri toplama/Basamak değeri toplama.cpp
@@ -0,0 +1,50 @@
+﻿// Basamak değeri toplama.cpp : Bu dosya 'main' işlevi içeriyor. Program yürütme orada başlayıp biter.
+//
+
+#include <iostream>
+using namespace std;
+
+int sum_digits(int value)
+{
+	int remainder[10];
+	int counter = 0;
+
+	while (value > 0)
+	{
+	
+		remainder[counter] = value % 10;
+		value /= 10; 
+
+		counter++;
+
+		if (value < 10)
+		{
+
+			remainder[counter] = value;
+			
+		}
+
+
+	}
+	
+	int total = 0;
+	for (int i = 0; i < counter; i++)
+	{
+		total +=remainder[i];
+	}
+
+	return total;
+}
+
+int main()
+{
+	int number;
+	cout << "enter the  a number";
+	cin >> number;
+
+	cout << number << "  the sum of the digits of the number  " << sum_digits(number)<<endl;
+
+
+
+
+}
