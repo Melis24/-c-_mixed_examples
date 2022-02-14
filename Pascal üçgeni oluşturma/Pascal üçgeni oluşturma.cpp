a1126fcca20cd86294f144a3b0f652535eba1c8c
Melis24 <77929541+Melis24@users.noreply.github.com>
2022-02-14 19:39:56 +0300

    Pacsal ucgeni olusturma

---
+++ Pascal üçgeni oluşturma/Pascal üçgeni oluşturma.cpp
@@ -0,0 +1,46 @@
+﻿// Pascal üçgeni oluşturma.cpp : Bu dosya 'main' işlevi içeriyor. Program yürütme orada başlayıp biter.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int linesnumb, number = 1, space;
+
+	cout << "enter number of lines";
+	cin >> linesnumb;
+
+	for (int i = 0; i < linesnumb; i++)
+	{
+
+		for (int space = 1; space < linesnumb - i; space++)
+		{
+			cout << " ";
+
+
+		}
+		for (int j = 0; j <= i; j++)
+		{
+
+			if (j == 0 || i == 0)
+			{
+				number = 1;
+			}
+			else
+			{
+				number = number * (i - j + 1) / j;
+			}
+
+			cout << number;
+		}
+
+		cout << endl;
+
+
+
+	}
+
+
+}
+
