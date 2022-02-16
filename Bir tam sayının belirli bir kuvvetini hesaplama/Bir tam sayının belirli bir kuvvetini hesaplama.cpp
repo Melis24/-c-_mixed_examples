bd7a6b2b986130660c6c1311ffbaae762824bfc6
Melis24 <77929541+Melis24@users.noreply.github.com>
2022-02-16 15:33:06 +0300

    Bir tamsayının belirli bir kuvvetini hesaplama

---
+++ Bir tam sayının belirli bir kuvvetini hesaplama/Bir tam sayının belirli bir kuvvetini hesaplama.cpp
@@ -0,0 +1,27 @@
+﻿
+#include <iostream>
+using namespace std;
+
+int power(unsigned int base, unsigned int exp)
+{
+    int result = 1;
+
+    for (int i = 1; i <= exp; i++)
+    {
+        result *= base;
+    }
+
+
+    return result;
+
+}
+
+int main()
+{
+    int base, exp;
+
+    cout << "enter base and exponent values";
+    cin >> base >> exp;
+    cout << "your number " << exp << " strength " << power(base, exp);
+
+}
