54c5ac9b1ed537628faed19521a587a7aadc2ed7
Melis24 <77929541+Melis24@users.noreply.github.com>
2022-02-17 15:30:12 +0300

    Dizi sıralı mı

---
+++ Dizi sıralı mı/Dizi sıralı mı.cpp
@@ -0,0 +1,51 @@
+﻿
+#include <iostream>
+using namespace std;
+
+int is_sorted(int arr[], int size)
+{
+    for (int i = 0; i < size; i++) // 1 2 5 4 6
+    {
+        for (int j = i; j < size; j++) 
+        {
+            if (arr[i] < arr[j])
+            {
+                return 1;
+            }
+            else
+            {
+                return 0;
+                break;
+            }
+
+        }
+
+    }
+
+
+}
+
+
+int main()
+{
+    int array[5], n;
+    n = 5;
+
+    cout << "enter the elements of the array";
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> array[i];
+    }
+
+    if (is_sorted(array, n))
+    {
+        cout << "array sequential";
+    }
+    else
+    {
+        cout << "array is not sequential";
+    }
+
+}
+
